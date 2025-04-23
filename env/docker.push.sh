b6d068238e0ea534843dd0e254ff409e096e2f4b
Tarsis Lima <br.tmvdl@gmail.com>
2025-04-22 22:09:18 -0300

    Update Docker push command and remove obsolete tag script

--- env/docker.push.sh
+++ env/docker.push.sh
@@ -1,1 +1,1 @@
-docker push 344104490154.dkr.ecr.us-east-1.amazonaws.com/workana/project1:latest
+docker push tmvdl/php:laravel
--- env/docker.tag.sh
+++
@@ -1,1 +0,0 @@
-docker tag workana/project1:latest 344104490154.dkr.ecr.us-east-1.amazonaws.com/workana/project1:latest
