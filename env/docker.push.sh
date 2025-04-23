ccaef96aa97395fe7668e7e32b8f0922ae79373b
Tarsis Lima <br.tmvdl@gmail.com>
2025-04-22 21:47:56 -0300

    Add Docker environment scripts and remove obsolete git scripts

---
+++ env/docker.push.sh
@@ -0,0 +1,1 @@
+docker push 344104490154.dkr.ecr.us-east-1.amazonaws.com/workana/project1:latest
---
+++ env/docker.tag.sh
@@ -0,0 +1,1 @@
+docker tag workana/project1:latest 344104490154.dkr.ecr.us-east-1.amazonaws.com/workana/project1:latest
